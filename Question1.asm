649b33faf0c1eddc5d89d9cd7455b5ff121a938c
TanishTale <tanishtale30@gmail.com>
2026-02-03 21:06:37 +0530

    Add files via upload

---
+++ Question1.asm
@@ -0,0 +1,16 @@
+ORG 0000H
+MOV A, #00H
+MOV B, #00H
+
+INC A     
+MOV B, #0AH
+MUL AB 
+ADD A, #00H
+MOV B, #0AH
+MUL AB         
+ADD A, #06H
+MOV B, #0AH
+MUL AB           
+ADD A, #03H   
+
+END: SJMP END
